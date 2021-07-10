74abe6d500660135778b73c9dedc4d37ad996b00
Travis Reeder <treeder@gmail.com>
2021-07-10 10:53:01 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Extract private key from keystore
+
+* Clone this repo
+* copy keystore JSON to cloned directory as `keystore.json`
+* then:
+
+```sh
+npm install
+node extract.js PASSWORD
+```
---
+++ extract.js
@@ -0,0 +1,10 @@
+var keyth = require('keythereum')
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('keystore.json');
+let keyobj = JSON.parse(rawdata);
+
+let password = process.argv[2];
+let privateKey = keyth.recover(password, keyobj)
+
+console.log(privateKey.toString('hex'))
